611e64f71ae45a78a2f1eb19a3dba8da1ccea037
Yan Pozo <yandry@urban4m.com>
2015-05-19 14:53:54 -0400

    Solved ide-identifying-comments

--- regexp/ide-identifying-comments/main.go
+++ regexp/ide-identifying-comments/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"os"
 	"regexp"
 	"strings"
@@ -11,13 +13,53 @@ var regEx = regexp.MustCompile(`/\*`)
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	var flag_is_commented = false
+	var buffer bytes.Buffer
+
 	for scanner.Scan() {
 		var line string = strings.TrimSpace(scanner.Text())
+
 		var ixslashslash int = strings.Index(line, "//")
-		if ixslashslash >= 0 {
-			slashcomments = append(slashcomments, ixslashslash+count)
+		var ixasterix int = strings.Index(line, "/*")
+
+		if flag_is_commented {
+			var ixclosecomm int = strings.Index(line, "*/")
+			if ixclosecomm >= 0 {
+				buffer.WriteString(line[0 : ixclosecomm+2])
+
+				fmt.Println(buffer.String())
+
+				flag_is_commented = false
+				buffer.Reset()
+
+			} else {
+				buffer.WriteString(line)
+				buffer.WriteString("\n")
+				continue
+			}
 		}
 
+		if ixslashslash >= 0 && !flag_is_commented && (ixslashslash < ixasterix || ixasterix < 0) {
+			fmt.Println(line[ixslashslash:])
+			continue
+		}
+
+		if ixasterix >= 0 {
+			buffer.WriteString(line[ixasterix:])
+			buffer.WriteString("\n")
+			flag_is_commented = true
+
+			var ixclosecomm int = strings.Index(line, "*/")
+			if ixclosecomm >= 0 {
+				flag_is_commented = false
+				fmt.Println(line[ixasterix : ixclosecomm+2])
+				buffer.Reset()
+
+				if ixslashslash >= 0 {
+					fmt.Println(line[ixslashslash:])
+				}
+			}
+		}
 	}
 
 }
