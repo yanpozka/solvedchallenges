229c68ee453656d8af95c6c2a2a890e14ddea09b
Yan <ypozoka@gmail.com>
2015-07-24 21:41:52 -0400

    Dynamic Coin change

---
+++ dynamic-programming/coin-change/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	N, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	M, _ := strconv.Atoi(scanner.Text())
+
+	coins := make([]int64, M)
+	nways := make([]int64, N+1)
+
+	for ix := 0; ix < M && scanner.Scan(); ix++ {
+		x, _ := strconv.ParseInt(scanner.Text(), 10, 0)
+		coins[ix] = x
+	}
+
+	NN := int64(N)
+	nways[0] = 1
+	for ix := 0; ix < M; ix++ {
+		c := coins[ix]
+		for k := c; k <= NN; k++ {
+			nways[k] += nways[k-c]
+		}
+	}
+
+	fmt.Println(nways[N])
+}
