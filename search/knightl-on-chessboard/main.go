57576f57e57665a466812563144f49a716958063
Yan <ypozoka@gmail.com>
2017-05-20 14:57:14 -0700

    BFS solution for knightl-on-chessboard

--- search/knightl-on-chessboard/main.go
+++ search/knightl-on-chessboard/main.go
@@ -1,109 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
 func main() {
 	var n int
 	fmt.Scanf("%d", &n)
 
-	solution(n)
-}
-
-func solution(n int) {
-
 	for r := 1; r < n; r++ {
 		for c := 1; c < n; c++ {
-			// fmt.Print(search(n, r, c), " ")
-			search(n, r, c)
-		}
-		// fmt.Println()
-	}
-}
-
-func search(n, s, e int) int {
-	b := make([][]bool, n)
-	for ix := range b {
-		b[ix] = make([]bool, n)
-	}
-
-	res := dfs(b, n, s, e, 0)
-	for r := 1; r < n; r++ {
-		for c := 1; c < n; c++ {
-			fmt.Print(b[r][c], " ")
+			fmt.Print(bfs(n, r, c))
+			if c < n-1 {
+				fmt.Print(" ")
+			}
 		}
 		fmt.Println()
 	}
-
-	return res
 }
 
-func dfs(board [][]bool, n, r, c, count int) int {
-	if r == n-1 && c == n-1 {
-		return count + 1
-	}
-	fmt.Println(r, c, count)
-
-	board[r][c] = true
-	a, b := r, c
-
-	// r +/- a
-	// c +/- b
-	if n_r, n_c := r+a, c+b; n_r < n && n_c < n && board[n_r][n_c] == false {
-		if result := dfs(duplicate(board), n, n_r, n_c, count+1); result != -1 {
-			return result
-		}
-	}
-	if n_r, n_c := r+a, c-b; n_r < n && n_c >= 0 && board[n_r][n_c] == false {
-		if result := dfs(duplicate(board), n, n_r, n_c, count+1); result != -1 {
-			return result
-		}
-	}
-	if n_r, n_c := r-a, c+b; n_r >= 0 && n_c < n && board[n_r][n_c] == false {
-		if result := dfs(duplicate(board), n, n_r, n_c, count+1); result != -1 {
-			return result
-		}
-	}
-	if n_r, n_c := r-a, c-b; n_r >= 0 && n_c >= 0 && board[n_r][n_c] == false {
-		if result := dfs(duplicate(board), n, n_r, n_c, count+1); result != -1 {
-			return result
-		}
+func bfs(n, src_r, src_c int) int {
+	board := make([][]bool, n)
+	for ix := range board {
+		board[ix] = make([]bool, n)
 	}
 
-	// r +/- b
-	// c +/- a
-	if n_r, n_c := r+b, c+a; n_r < n && n_c < n && board[n_r][n_c] == false {
-		if result := dfs(duplicate(board), n, n_r, n_c, count+1); result != -1 {
-			return result
+	board[src_r][src_c] = true
+	a, b := src_r, src_c
+
+	q := list.New()
+	q.PushBack(&pair{R: src_r, C: src_c, Level: 1})
+
+	for q.Len() > 0 {
+		p := q.Remove(q.Front()).(*pair)
+		if p.R == n-1 && p.C == n-1 {
+			return p.Level
 		}
-	}
-	if n_r, n_c := r+b, c-a; n_r < n && n_c >= 0 && board[n_r][n_c] == false {
-		if result := dfs(duplicate(board), n, n_r, n_c, count+1); result != -1 {
-			return result
+		r, c := p.R, p.C
+
+		// r +/- a
+		// c +/- b
+		if n_r, n_c := r+a, c+b; n_r < n && n_c < n && board[n_r][n_c] == false {
+			board[n_r][n_c] = true
+			q.PushBack(&pair{R: n_r, C: n_c, Level: p.Level + 1})
 		}
-	}
-	if n_r, n_c := r-b, c+a; n_r >= 0 && n_c < n && board[n_r][n_c] == false {
-		if result := dfs(duplicate(board), n, n_r, n_c, count+1); result != -1 {
-			return result
+		if n_r, n_c := r+a, c-b; n_r < n && n_c >= 0 && board[n_r][n_c] == false {
+			board[n_r][n_c] = true
+			q.PushBack(&pair{R: n_r, C: n_c, Level: p.Level + 1})
 		}
-	}
-	if n_r, n_c := r-b, c-a; n_r >= 0 && n_c >= 0 && board[n_r][n_c] == false {
-		if result := dfs(duplicate(board), n, n_r, n_c, count+1); result != -1 {
-			return result
+		if n_r, n_c := r-a, c+b; n_r >= 0 && n_c < n && board[n_r][n_c] == false {
+			board[n_r][n_c] = true
+			q.PushBack(&pair{R: n_r, C: n_c, Level: p.Level + 1})
+		}
+		if n_r, n_c := r-a, c-b; n_r >= 0 && n_c >= 0 && board[n_r][n_c] == false {
+			board[n_r][n_c] = true
+			q.PushBack(&pair{R: n_r, C: n_c, Level: p.Level + 1})
+		}
+
+		// r +/- b
+		// c +/- a
+		if n_r, n_c := r+b, c+a; n_r < n && n_c < n && board[n_r][n_c] == false {
+			board[n_r][n_c] = true
+			q.PushBack(&pair{R: n_r, C: n_c, Level: p.Level + 1})
+		}
+		if n_r, n_c := r+b, c-a; n_r < n && n_c >= 0 && board[n_r][n_c] == false {
+			board[n_r][n_c] = true
+			q.PushBack(&pair{R: n_r, C: n_c, Level: p.Level + 1})
+		}
+		if n_r, n_c := r-b, c+a; n_r >= 0 && n_c < n && board[n_r][n_c] == false {
+			board[n_r][n_c] = true
+			q.PushBack(&pair{R: n_r, C: n_c, Level: p.Level + 1})
+		}
+		if n_r, n_c := r-b, c-a; n_r >= 0 && n_c >= 0 && board[n_r][n_c] == false {
+			board[n_r][n_c] = true
+			q.PushBack(&pair{R: n_r, C: n_c, Level: p.Level + 1})
 		}
 	}
 
 	return -1
 }
 
-func duplicate(b [][]bool) [][]bool {
-	return b
-
-	// new_b := make([][]bool, len(b))
-	// for r := range b {
-	// 	new_b[r] = make([]bool, len(b))
-	// 	for c := range b {
-	// 		new_b[r][c] = b[r][c]
-	// 	}
-	// }
-	// return new_b
+type pair struct {
+	R, C, Level int
 }
