a6cafd176b6661c13394bebee8b7facdbd5173fe
Yandry Pozo <ypozoka@gmail.com>
2018-01-15 18:29:20 -0800

    Union/find with extra data

---
+++ leetcode/redundant-connection/main.go
@@ -0,0 +1,100 @@
+// https://leetcode.com/problems/redundant-connection/
+//
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	acc := [][]string{
+		{"John", "johnsmith@mail.com", "john00@mail.com"},
+		{"John", "johnnybravo@mail.com"},
+		{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+		{"Mary", "mary@mail.com"},
+	}
+	fmt.Println(accountsMerge(acc))
+}
+
+func accountsMerge(accounts [][]string) [][]string {
+	initTree(len(accounts))
+	m := make(map[string]int)
+
+	for ix, person := range accounts {
+		for k := 1; k < len(person); k++ {
+			email := person[k]
+			if p, contains := m[email]; contains {
+				union(p, ix)
+			}
+			m[email] = ix
+		}
+	}
+
+	ids := make(map[int]map[string]bool, len(accounts))
+
+	for ix := range tree {
+		p := find(ix)
+		if _, contains := ids[p]; !contains {
+			ids[p] = make(map[string]bool)
+		}
+		emails := ids[p]
+		if ix == p {
+			person := accounts[p]
+			for k := 1; k < len(person); k++ {
+				emails[person[k]] = true
+			}
+		} else {
+			person := accounts[ix]
+			for k := 1; k < len(person); k++ {
+				emails[person[k]] = true
+			}
+		}
+		ids[p] = emails
+	}
+
+	res := make([][]string, 0, len(ids))
+
+	for ix, emails := range ids {
+		es := make([]string, 0, len(emails))
+		for email := range emails {
+			es = append(es, email)
+		}
+		sort.Strings(es)
+		res = append(res, append([]string{accounts[ix][0]}, es...))
+	}
+
+	return res
+}
+
+var tree, rank []int
+
+func initTree(n int) {
+	tree = make([]int, n)
+	for ix := range tree {
+		tree[ix] = ix
+	}
+	rank = make([]int, n)
+}
+
+func find(r int) int {
+	if tree[r] == r {
+		return r
+	}
+	return find(tree[r])
+}
+
+func union(x, y int) {
+	pX, pY := find(x), find(y)
+	if pX == pY {
+		return
+	}
+	if rank[pX] > rank[pY] {
+		tree[pY] = pX
+	} else {
+		if rank[pX] == rank[pY] {
+			rank[pY]++
+		}
+		tree[pX] = pY
+	}
+}
