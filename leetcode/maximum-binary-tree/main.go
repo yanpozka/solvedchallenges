debc69a8d05da7febed944d6b5f2fa1168f29b4d
Yandry Pozo <ypozoka@gmail.com>
2021-12-04 18:59:05 -0700

    Another solution for maximum-binary-tree leetcode

--- leetcode/maximum-binary-tree/main.go
+++ leetcode/maximum-binary-tree/main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	root := constructMaximumBinaryTree([]int{1, 3, 2})
-	fmt.Println(root.Val, root.Left.Val, root.Right.Val)
+	{
+		root := constructMaximumBinaryTree([]int{1, 3, 2})
+		fmt.Println(root.Val, root.Left.Val, root.Right.Val)
+	}
+	{
+		root := constructMaximumBinaryTree([]int{3, 2, 1, 6, 0, 5})
+		fmt.Println(root.Val, root.Left.Val, root.Right.Val)
+	}
 }
 
 type TreeNode struct {
@@ -32,5 +38,33 @@ func constructMaximumBinaryTree(nums []int) *TreeNode {
 		Val:   max,
 		Left:  constructMaximumBinaryTree(nums[:maxIx]),
 		Right: constructMaximumBinaryTree(nums[maxIx+1:]),
+	}
+}
+
+func constructMaximumBinaryTreeNonSlicing(nums []int) *TreeNode {
+	return constructMaximumBT(nums, 0, len(nums)-1)
+}
+
+func constructMaximumBT(nums []int, start, end int) *TreeNode {
+	if start == end {
+		return &TreeNode{Val: nums[start]}
+	}
+	fmt.Println(start, end)
+	if start > end {
+		return nil
+	}
+
+	var max, maxIx int
+	for ix := start; ix <= end; ix++ {
+		if nums[ix] > max {
+			max = nums[ix]
+			maxIx = ix
+		}
+	}
+	fmt.Println(max)
+	return &TreeNode{
+		Val:   max,
+		Left:  constructMaximumBT(nums, start, maxIx-1),
+		Right: constructMaximumBT(nums, maxIx+1, end),
 	}
 }
