57f3aed2b53b2d5ac9965e741307a80100c0aafe
Yandry Pozo <ypozoka@gmail.com>
2021-12-17 13:25:27 -0700

    Reduce solution for majority-element

--- leetcode/majority-element/main.go
+++ leetcode/majority-element/main.go
@@ -8,14 +8,10 @@ func main() {
 
 func majorityElement(nums []int) int {
 	freqs := map[int]int{}
-
+	var res int
 	for _, n := range nums {
 		freqs[n]++
-	}
-
-	var res int
-	for n, f := range freqs {
-		if f > len(nums)/2 {
+		if freqs[n] > len(nums)/2 {
 			res = n
 			break
 		}
